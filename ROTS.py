3cb53789d2a8e696a7fa7f57bed1d49b5ebbb32f
unknown <Nazli Kaplankiran>
2025-10-01 15:21:43 -0700

    updating file

--- ROTS.py
+++ ROTS.py
@@ -5,9 +5,8 @@ import time
 
 def printLines():
     lines = [
-        ("I have brought peace................freedom................justice...........and security to my new empire.", 0.06, 1.0),                # pause 1 second after this line
-        ("Your new Empire?", 0.05, 1.0), # pause 0.5 seconds
-        ("Don't make me kill you.", 0.07, 1.0),                # pause 2 seconds
+        ("I have brought peace................freedom................justice...........and security to my new empire.", 0.06, 1.0),          
+        ("Don't make me kill you.", 0.07, 1.0),              
         ("Anakin...........my allegiance is to the Republic! To Demoncracy!", 0.06, 1.0),
         ("If you're not with me......then you're my enemy.", 0.06, 1.0),
         ("Only a Sith deals in absolutes............I will do what I must.", 0.07, 1.0),
